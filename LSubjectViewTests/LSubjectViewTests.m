4823d0c8f953cbd2c0bc95a15be3842fe488029e
littlelightwang <xiaoliangzi@xiaoliangzi.local>
2014-08-23 21:24:18 +0800

    未命名
    iOS demo

---
+++ LSubjectViewTests/LSubjectViewTests.m
@@ -0,0 +1,34 @@
+//
+//  LSubjectViewTests.m
+//  LSubjectViewTests
+//
+//  Created by 王小亮 on 14/8/22.
+//  Copyright (c) 2014年 WXL. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LSubjectViewTests : XCTestCase
+
+@end
+
+@implementation LSubjectViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
